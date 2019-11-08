5e2ed4f04066627478cb2c6d53cfa87994833436
navkk <navkkrnair@gmail.com>
2019-11-08 16:36:40 +0530

    Changed and added readiness probe

--- src/main/java/com/istio/CustomerApplication.java
+++ src/main/java/com/istio/CustomerApplication.java
@@ -1,7 +1,13 @@
 package com.istio;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -9,10 +15,33 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class CustomerApplication
 {
+	private static final Logger logger = LoggerFactory.getLogger(CustomerApplication.class);
 
 	public static void main(String[] args)
 	{
-		SpringApplication.run(CustomerApplication.class, args);
+		SpringApplication app = new SpringApplication(CustomerApplication.class);
+		app.addListeners((e) ->
+		{
+			if (e instanceof ApplicationReadyEvent)
+			{
+				File file = new File(File.separatorChar + "var" + File.separatorChar + "ready");
+				logger.info("Creating file at: " + file);
+				if (!(file.exists()))
+				{
+					try
+					{
+						file.createNewFile();
+						logger.info("created file: " + file);
+					}
+					catch (IOException e1)
+					{
+						logger.info("Creating file failed");
+					}
+
+				}
+			}
+		});
+		app.run(args);
 	}
 
 	@Bean
