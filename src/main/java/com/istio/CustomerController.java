0e29811d01c7e7ca89924d35b3393cad754468cc
navkkrnair <navkkrnair@gmail.com>
2019-11-12 13:43:01 +0530

    Changed code a little

--- src/main/java/com/istio/CustomerController.java
+++ src/main/java/com/istio/CustomerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,46 +41,33 @@ public class CustomerController
 			@RequestHeader(value = "user-preference", required = false) String userPreference)
 	{
 		logger.info(">> getCustomer() called");
-		try
+
+		tracer.activeSpan().setBaggageItem("user-agent", userAgent);
+		if (userPreference != null && !userPreference.isEmpty())
 		{
-			/**
-			 * Set baggage
-			 */
-			tracer.activeSpan().setBaggageItem("user-agent", userAgent);
-			if (userPreference != null && !userPreference.isEmpty())
-			{
-				tracer.activeSpan().setBaggageItem("user-preference", userPreference);
-			}
-			logger.info(">> Calling {}", remoteURL);
-			ResponseEntity<String> responseEntity = restTemplate.getForEntity(remoteURL, String.class);
-			String                 response       = responseEntity.getBody();
-			return ResponseEntity.ok(String.format(RESPONSE_STRING_FORMAT, response.trim()));
+			tracer.activeSpan().setBaggageItem("user-preference", userPreference);
 		}
-		catch (HttpStatusCodeException ex)
-		{
-			logger.warn("Exception trying to get the response from preference service.", ex);
-			return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
-				.body(
-						String.format(
-								RESPONSE_STRING_FORMAT,
-									String.format("%d %s", ex.getRawStatusCode(), createHttpErrorResponseString(ex))));
-		}
-		catch (RestClientException ex)
-		{
-			logger.warn("Exception trying to get the response from preference service.", ex);
-			return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
-				.body(String.format(RESPONSE_STRING_FORMAT, ex.getMessage()));
-		}
+		logger.info(">> Calling {}", remoteURL);
+		ResponseEntity<String> responseEntity = restTemplate.getForEntity(remoteURL, String.class);
+		String                 response       = responseEntity.getBody();
+		return ResponseEntity.ok(String.format(RESPONSE_STRING_FORMAT, response.trim()));
+
+	}
+
+	@ExceptionHandler
+	public ResponseEntity<String> handleException(HttpStatusCodeException ex)
+	{
+		return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+			.body(
+					String.format(
+							RESPONSE_STRING_FORMAT,
+								String.format("%d %s", ex.getRawStatusCode(), createHttpErrorResponseString(ex))));
 	}
 
 	private String createHttpErrorResponseString(HttpStatusCodeException ex)
 	{
-		String responseBody = ex.getResponseBodyAsString().trim();
-		if (responseBody.startsWith("null"))
-		{
-			return ex.getStatusCode().getReasonPhrase();
-		}
-		return responseBody;
+		return ex.getResponseBodyAsString().trim();
+
 	}
 
 }
